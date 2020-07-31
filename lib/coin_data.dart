8f40321a22c847cdb79eb64db49cfa762431c935
jayesh312001 <jayesh312001@gmail.com>
2020-07-31 18:53:46 +0530

    final commit, removed redundant classes

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -10,7 +13,7 @@ const List<String> currenciesList = [
   'ILS',
   'INR',
   'JPY',
-  'MXN', 
+  'MXN',
   'NOK',
   'NZD',
   'PLN',
@@ -21,11 +24,33 @@ const List<String> currenciesList = [
   'USD',
   'ZAR'
 ];
-
 const List<String> cryptoList = [
   'BTC',
   'ETH',
   'LTC',
 ];
+const kapikey = 'A811D413-7C62-465F-B058-0D0802F0BDCB';
+const kapikey2 = 'A8000716-2296-4081-B97D-1F1D7F09745D';
+const url = 'https://rest.coinapi.io/v1/exchangerate';
+Map<String, String> priceData = {};
 
-class CoinData {}
+class CoinData {
+  //Function to get data from the API
+  Future<dynamic> getData(String selectedCurrency) async {
+    try {
+      for (String crypto in cryptoList) {
+        http.Response response =
+            await http.get('$url/$crypto/$selectedCurrency?apikey=$kapikey2');
+        if (response.statusCode == 200) {
+          var data = jsonDecode(response.body);
+          priceData[crypto] = data['rate'].toStringAsFixed(0);
+        } else {
+          print('Error ${response.statusCode}');
+        }
+      }
+      return priceData;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,12 +1,7 @@
 import 'package:bitcoin_ticker/coin_data.dart';
-import 'package:bitcoin_ticker/utilities/android_picker.dart';
-import 'package:bitcoin_ticker/utilities/ios_picker.dart';
-import 'utilities/constants.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:io' show Platform;
-import 'network/networking.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -14,16 +9,27 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String btc = '?';
-  String ltc = '?';
-  String eth = '?';
   String selectedCurrency = 'INR';
+  void initState() {
+    super.initState();
+    getCoinData();
+  }
 
-  Future<dynamic> getPriceData(String cryptoCurrency) async {
-    NetworkHelper networkHelper = NetworkHelper(
-        url: '$url/$cryptoCurrency/$selectedCurrency?apikey=$kapikey');
-    var priceData = await networkHelper.getData();
-    updateUI(priceData);
+  bool isloading = false;
+  Map cryptoPricePair = {};
+  void getCoinData() async {
+    try {
+      isloading = true;
+      var data = await CoinData().getData(selectedCurrency);
+      isloading = false;
+      setState(
+        () {
+          cryptoPricePair = data;
+        },
+      );
+    } catch (e) {
+      print(e);
+    }
   }
 
   DropdownButton<String> getAndroidPicker() {
@@ -42,27 +48,27 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
-          getPriceData('BTC');
-          getPriceData('ETH');
-          getPriceData('LTC');
+          getCoinData();
         });
       },
     );
   }
 
-  void updateUI(dynamic priceData) {
-    setState(() {
-      String price = priceData['rate'].toStringAsFixed(2);
-      btc = price;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getPriceData('BTC');
-    getPriceData('ETH');
-    getPriceData('LTC');
+  CupertinoPicker getCupertinoPicker() {
+    List<Text> pickerItems = [];
+    for (String currencies in currenciesList) {
+      pickerItems.add(Text(currencies));
+    }
+    return CupertinoPicker(
+      itemExtent: 32,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+          getCoinData();
+        });
+      },
+      children: pickerItems,
+    );
   }
 
   @override
@@ -75,27 +81,32 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          CurrencyCard(
-            currentPrice: btc,
-            selectedCurrency: selectedCurrency,
-            selectedCryptoCurrency: 'BTC',
-          ),
-          CurrencyCard(
-            currentPrice: btc,
-            selectedCurrency: selectedCurrency,
-            selectedCryptoCurrency: 'ETH',
-          ),
-          CurrencyCard(
-            currentPrice: btc,
-            selectedCurrency: selectedCurrency,
-            selectedCryptoCurrency: 'LTC',
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              CryptoCard(
+                crypto: 'BTC',
+                value: isloading ? '?' : cryptoPricePair['BTC'],
+                selectedCurrency: selectedCurrency,
+              ),
+              CryptoCard(
+                crypto: 'ETH',
+                value: isloading ? '?' : cryptoPricePair['ETH'],
+                selectedCurrency: selectedCurrency,
+              ),
+              CryptoCard(
+                crypto: 'LTC',
+                value: isloading ? '?' : cryptoPricePair['LTC'],
+                selectedCurrency: selectedCurrency,
+              ),
+            ],
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: Platform.isIOS ? IOSPicker() : getAndroidPicker(),
+            child: Platform.isIOS ? getCupertinoPicker() : getAndroidPicker(),
           ),
         ],
       ),
@@ -103,22 +114,22 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 }
 
-class CurrencyCard extends StatelessWidget {
-  const CurrencyCard({
+class CryptoCard extends StatelessWidget {
+  const CryptoCard({
     Key key,
-    @required this.selectedCryptoCurrency,
-    @required this.currentPrice,
     @required this.selectedCurrency,
+    @required this.value,
+    @required this.crypto,
   }) : super(key: key);
 
-  final String currentPrice;
   final String selectedCurrency;
-  final String selectedCryptoCurrency;
+  final String value;
+  final String crypto;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 18.0),
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0.0),
       child: Card(
         color: Colors.lightBlueAccent,
         elevation: 5.0,
@@ -128,7 +139,7 @@ class CurrencyCard extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
           child: Text(
-            '1 $selectedCryptoCurrency = $currentPrice $selectedCurrency',
+            '1 $crypto = $value $selectedCurrency',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 20.0,
